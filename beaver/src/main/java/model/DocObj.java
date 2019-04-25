264bcd8192cce62646b7ddc1f4614230a5f7895d
阎涵 <yanhan166@gmail.com>
2019-04-25 13:46:07 -0400

    test indexer commit

---
+++ beaver/src/main/java/model/DocObj.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class DocObj {
+    int id;
+
+    Map<String, List<Integer>> positions = new HashMap<>();
+    Map<String, Integer> freqs = new HashMap<>();
+
+    public DocObj(int id) {
+        this.id = id;
+    }
+
+    public void addOccurrence(String word, int pos) {
+        List<Integer> list = positions.get(word);
+        if(list == null) {
+            list = new ArrayList<>();
+        }
+        list.add(pos);
+        positions.put(word, list);
+    }
+
+    public void addFreq(String word, int weight) {
+        Integer val = freqs.get(word);
+        freqs.put(word, val != null? (val.intValue()+weight) : weight);
+    }
+
+    public Set<String> getAllWords() {
+        return positions.keySet();
+    }
+
+    public List<Integer> getPositions(String word) {
+        return positions.get(word);
+    }
+
+    public int getFreq(String word) {
+        if(freqs.containsKey(word)) {
+            return freqs.get(word);
+        } else {
+            return 0;
+        }
+    }
+}
