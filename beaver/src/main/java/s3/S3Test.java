2144fad86ffb29530abcee306a1104cd7ed48fc0
zhileiz <zhileiz@seas.upenn.edu>
2019-04-27 15:06:10 -0400

    integrated amazon s3

---
+++ beaver/src/main/java/s3/S3Test.java
@@ -0,0 +1,18 @@
+package s3;
+
+import s3.S3Service;
+import java.io.IOException;
+import java.util.List;
+
+public class S3Test {
+
+    public static void main(String[] args) throws IOException {
+        List<String> list  = S3Service.getInstance().listAllFiles("documents/1/");
+        for (String s : list) {
+            String content = S3Service.getInstance().getFileAsString(s);
+            System.out.println("===== File of length " + content.length() + "=====");
+            System.out.println(content.substring(0, 300));
+        }
+    }
+
+}
