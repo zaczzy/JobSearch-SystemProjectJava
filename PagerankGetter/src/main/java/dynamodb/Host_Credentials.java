f758aa439d5f85fb483c1b00f7be8095f4c230af
zhileiz <zhileiz@seas.upenn.edu>
2019-05-12 02:16:48 -0400

    merged pagerank-getter

---
+++ PagerankGetter/src/main/java/dynamodb/Host_Credentials.java
@@ -0,0 +1,10 @@
+package dynamodb;
+
+import software.amazon.awssdk.regions.Region;
+
+public class Host_Credentials {
+    public static final String TABLE_NAME = "test2";
+    public static final Region REGION = Region.US_EAST_1;
+    public static final String ACCESS_KEY = "***REMOVED***";
+    public static final String SECRET_KEY = "***REMOVED***";
+}
---
+++ PagerankGetter/src/test/java/TestPageRankGetter.java
@@ -0,0 +1,14 @@
+import dynamodb.PageRankGetter;
+import org.junit.Test;
+
+public class TestPageRankGetter {
+    @Test
+    public void test() {
+        PageRankGetter.get("01bd1190-0975-4061-998c-0bc46bc576b9");
+    }
+
+    @Test
+    public void testAll() throws InterruptedException {
+        PageRankGetter.getAll();
+    }
+}
